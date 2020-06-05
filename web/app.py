914e16326b6cb1a372f32d35c0b3cd0f61a23893
hoto17296 <mail@hoto.me>
2020-06-06 01:50:53 +0900

    connect to redis

--- web/app.py
+++ web/app.py
@@ -1,8 +1,12 @@
 from os import environ
+import asyncio
 from aiohttp import web
 import aiohttp_jinja2
 import jinja2
 import asyncpg
+import aiohttp_session
+from aiohttp_session.redis_storage import RedisStorage
+import aioredis
 from views.index import routes as routes_index
 
 
@@ -16,9 +20,14 @@ async def cleanup(app: web.Application):
 
 app = web.Application()
 
+
 app["static_root_url"] = environ["STATIC_ROOT_URL"]
 
 aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("templates"))
+
+coro = aioredis.create_pool(environ["REDIS_URL"])
+redis = asyncio.get_event_loop().run_until_complete(coro)
+aiohttp_session.setup(app, RedisStorage(redis))
 
 app.add_routes(routes_index)
 
--- web/main.py
+++ web/main.py
@@ -7,6 +7,7 @@ import uvloop
 from aiohttp import web
 from app import app
 
+uvloop.install()
+
 if __name__ == "__main__":
-    uvloop.install()
     web.run_app(app)
