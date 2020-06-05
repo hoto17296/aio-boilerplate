25621a5eef098c0672de97a0f18dbdeeb64fc6c8
hoto17296 <mail@hoto.me>
2020-06-06 00:38:30 +0900

    mv app

--- web/app.py
+++ web/app.py
@@ -2,7 +2,7 @@ from os import environ
 from aiohttp import web
 import aiohttp_jinja2
 import jinja2
-from .views.index import routes as routes_index
+from views.index import routes as routes_index
 
 app = web.Application()
 
